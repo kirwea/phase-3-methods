09f6e136f64a43271e4c062de0375935a4b9dd0c
dennistaiti <dennistaiti7@gmail.com>
2023-02-08 15:42:55 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+
+def greet_programmer(name="programmer")
+    puts "Hello, programmer!"
+end
+
+def greet(name="Naureen")
+    puts "Hello, #{name}!"
+end
+greet()
+
+def greet_with_default (name='programmer')
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+def halve (integer)
+    if integer.class == Integer
+    integer/2
+    else
+        nil
+    end
+end
